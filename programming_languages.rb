b80cdc3a976046f3948a4b2ec0c77571875b9bc9
Natasha Chernis <natashachernis@gmail.com>
2019-03-08 03:48:44 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@ def reformat_languages(languages)
 
   languages.each {|style, languages|
     languages.each {|language, attribute|
-      binding.pry
+      
       if new_language_hash[language].class == Hash
         new_language_hash[language][:style] << style
       else
